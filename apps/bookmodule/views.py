1f9c871d4d811d78abc2f6e4217ad53b686a840f
mohamad <mohamad-077@hotmail.com>
2024-03-16 00:05:29 +0300

    edit view

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render,redirect 
 def index(request):
      return render(request, 'bookmodule/index.html') # rendring a template
+def names(request):
+     u = request.user
+     return render(request, 'bookmodule/namesList.html')
+     #return redirect('/')
