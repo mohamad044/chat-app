d749c6fdb39f28364b13e601384b19c7e2f80cae
mohamad <mohamad-077@hotmail.com>
2024-03-16 00:05:01 +0300

    add another path

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from apps.bookmodule import views
 urlpatterns = [
- path('',views.index,name='index')
+ path('',views.index,name='index'),
+ path('names',views.names)
+ 
 ]
