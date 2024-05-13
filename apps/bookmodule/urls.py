51331be73f49f155f6484c34440ffa0d6c4f1080
mohamad044 <mo.alharbi077@gmail.com>
2024-05-13 16:57:07 +0300

    FINAL CHAT WEB (before was practice from the lectures )

--- apps/bookmodule/urls.py
+++
@@ -1,9 +0,0 @@
-# test
-from django.urls import path
-from apps.bookmodule import views
-urlpatterns = [
- path('',views.index,name='index'),
- path('books',views.books),
- path('book/<int:bId>',views.book)
- 
-]
